4a5f50273f2ee62379a9e7a39bf225a675669704
Anand Raja <meetanandns@gmail.com>
2026-08-04 08:08:13 +0530

    android sqlite fix

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,22 +3,6 @@ export {};
 declare global {
   interface Window {
     readonly Capacitor?: {
-      readonly Plugins?: {
-        readonly CapacitorSQLite?: {
-          isConnection(options: Record<string, unknown>): Promise<{ readonly result?: boolean }>;
-          createConnection(options: Record<string, unknown>): Promise<void>;
-          isDBOpen(options: Record<string, unknown>): Promise<{ readonly result?: boolean }>;
-          open(options: Record<string, unknown>): Promise<void>;
-          execute(options: Record<string, unknown>): Promise<unknown>;
-          query(
-            options: Record<string, unknown>,
-          ): Promise<{ readonly values?: readonly unknown[] }>;
-          run(options: Record<string, unknown>): Promise<unknown>;
-          beginTransaction(options: Record<string, unknown>): Promise<unknown>;
-          commitTransaction(options: Record<string, unknown>): Promise<unknown>;
-          rollbackTransaction(options: Record<string, unknown>): Promise<unknown>;
-        };
-      };
       isNativePlatform?(): boolean;
       getPlatform?(): string;
     };
